ddb3e26b894ba9fc58d827e4f9263aed2ebec71e
JohnMigwi <johnmigwi101@gmail.com>
2024-02-07 21:10:41 +0300

    products

---
+++ src/Components/Listing/ProductList.jsx
@@ -0,0 +1,10 @@
+import Product from "./Products";
+function Real (Product){
+    const productElement = [];
+    for (const pro of pro)
+    return (
+        <>
+
+        </>
+    );
+};
---
+++ src/Components/Listing/Products.jsx
@@ -0,0 +1,17 @@
+function Product (){
+const Products = [
+    {id: 'p1', title: 'A Book', price: 59.99},
+    {id: 'p2', title: 'A Carpet', price: 129.49},
+    {id: 'p3', title: 'Another Book', price: 39.99},
+    {id: 'p1', title: 'A Book', price: 59.99},
+    {id: 'p2', title: 'A Carpet', price: 129.49},
+    {id: 'p3', title: 'Another Book', price: 39.99},
+    {id: 'p1', title: 'A Book', price: 59.99},
+    {id: 'p2', title: 'A Carpet', price: 129.49},
+    {id: 'p3', title: 'Another Book', price: 39.99},
+    {id: 'p1', title: 'A Book', price: 59.99},
+    {id: 'p2', title: 'A Carpet', price: 129.49},
+    {id: 'p3', title: 'Another Book', price: 39.99},
+   ];
+};
+export default Product;
