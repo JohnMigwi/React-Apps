ab9b9913c816f3b0600b8b997c6ce310e4249fad
JohnMigwi <johnmigwi101@gmail.com>
2024-02-20 13:57:10 +0300

    checked

--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,34 +1,21 @@
-import {useState, useRef} from "react";
+  import { useState } from "react";
+    
+    function Email() {
+        const [selected, setSelected] = useState(false);
 
-function Email (){
-    const [counter, setCounter] = useState(0);
-    const counter2 = useRef(1);
-
-
-    function handleCount(){
-        setCounter(counter => counter + 1);
-        counter2.current +=1;
-        
+        function handleSelecting(){
+            setSelected(selected =>!selected);
+        }
+      
+        return(
+            <>
+            <label>
+             <input type="checkbox" name="Material" checked={selected} onChange={handleSelecting} />
+             <span>check</span>
+            </label>
+            </>
+        );
     };
 
-    function handleDecrease(){
-        setCounter(prevCounter => prevCounter - 1);
-        counter2.current -=1;
-    };
- 
-    return (
-        <>
-         <div>
-            <button onClick={handleCount}>Increase</button>
-             <ul>
-                <li>Counter :{counter}</li>
-                <li>Counter :{counter2.current}</li>
-             </ul>
-             <button onClick={handleDecrease}>Decrease</button>
-         </div>
-        
-        </>
-    );
 
-};
 export default Email;
