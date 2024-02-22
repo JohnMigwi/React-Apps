0873697d814b048de15907af33e29ac3c59ea934
JohnMigwi <johnmigwi101@gmail.com>
2024-02-22 15:06:53 +0300

    refs

--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,20 +1,40 @@
-  import { useState } from "react";
+  import { useState, useRef } from "react";
     
     function Email() {
-        const [selected, setSelected] = useState(false);
 
-        function handleSelecting(){
-            setSelected(selected =>!selected);
-        }
+        const nameref = useRef();
+        const choiceref = useRef();
+
+
+        const currentref = nameref.current.value;
+        console.log("current name is"+ currentref);
+
+
+
+        const currentChoice = choiceref.current.value;
+        console.log("current choise:" + currentChoice);
+
+
+
+  
+   
       
         return(
-            <>
-            <label>
-             <input type="checkbox" name="Material" checked={selected} onChange={handleSelecting} />
-             <span>check</span>
-            </label>
-            </>
-        );
+        <section>
+        <label>Your name</label>
+         <input type="text" ref={nameref}/>
+         <label for="select">Choose a pet:</label>
+            <select name="pets" id="pet-select" ref={choiceref}>
+                <option value="">--Please choose an option--</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="hamster">Hamster</option>
+                <option value="parrot">Parrot</option>
+                <option value="spider">Spider</option>
+                <option value="goldfish">Goldfish</option>
+            </select>
+        </section>
+       );
     };
 
 
