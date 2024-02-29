9cd6b8da38d69445b6e03a4e64a4eec44e542368
JohnMigwi <johnmigwi101@gmail.com>
2024-02-29 15:15:01 +0300

    traces

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-import Test from './Components/Email.jsx';
-import MyName from './Components/Name.jsx';
-import Age from './Components/Age.jsx';
-import MainGoal from './Components/Goals/Main-Goal.jsx';
-//import EngineCalc from './Components/Calculator/EngineCalc.jsx';
-import CalcLogic from './Components/Calculator/AlllogicSigns.jsx';
-import MyFirstPortal from './Components/Portal.jsx';
-
 
 
 function App (){
