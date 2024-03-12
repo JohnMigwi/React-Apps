02e66fd5e5dfd49c469be3184e3afae94ab19a2c
JohnMigwi <johnmigwi101@gmail.com>
2024-03-12 12:07:14 +0300

    dependcy error fix

--- src/Components/Goals/trial.jsx
+++ src/Components/Goals/trial.jsx
@@ -11,22 +11,26 @@ function Trial() {
         const response = await fetch('https://randomuser.me/api/');
         const {results} = await response.json();
         setLoadedResults(results);
+        console.log(results);
       }
       catch(error){
         console.log("Error Fetching data", error);
       }
     }
      allResults();
+   
    },[])
 
   return (
     <div>
       <h1>All USERS FETCHED</h1>
       <ul>
-        {loadedResults.map(({name, cell,phone}, index) =>(
-          <li key={index}>
-            <h2>{name.first}</h2>
-          </li>
+        {loadedResults.map(({titile, name, cell,phone, picture}, index) =>(
+          <div key={index}>
+            <h2>{name.title} {name.first} {name.last}</h2>
+            <img src={picture.thumbnail} alt="User thumbnail" />
+            <div>{cell}</div>
+          </div>
         ))}
       </ul>    
     </div>
