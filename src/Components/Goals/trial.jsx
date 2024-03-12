6e3b35d4d8445e81dde188493723b883a1820a88
JohnMigwi <johnmigwi101@gmail.com>
2024-03-12 11:37:38 +0300

    use effect hook

--- src/Components/Goals/trial.jsx
+++ src/Components/Goals/trial.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+// 'https://jsonplaceholder.typicode.com/posts'
+import React, {useState, useEffect} from 'react'
 
-function trial() {
+function Trial() {
+    //handle fetch data 
+    const [loadedResults, setLoadedResults] = useState([]);
+
+   useEffect(() =>{ 
+    const allResults = async () =>{
+      try{
+        const response = await fetch('https://randomuser.me/api/');
+        const {results} = await response.json();
+        setLoadedResults(results);
+      }
+      catch(error){
+        console.log("Error Fetching data", error);
+      }
+    }
+     allResults();
+   },[])
+
   return (
-    <div>trial</div>
+    <div>
+      <h1>All USERS FETCHED</h1>
+      <ul>
+        {loadedResults.map(({name, cell,phone}, index) =>(
+          <li key={index}>
+            <h2>{name.first}</h2>
+          </li>
+        ))}
+      </ul>    
+    </div>
+   
+   
   )
 }
 
-export default trial
+export default Trial  
+
