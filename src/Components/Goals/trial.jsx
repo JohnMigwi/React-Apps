fd16bd3938d8b3b9169c654ed60eb49f642f2882
JohnMigwi <johnmigwi101@gmail.com>
2024-03-12 19:55:47 +0300

    to fix error on query parameter

--- src/Components/Goals/trial.jsx
+++ src/Components/Goals/trial.jsx
@@ -1,38 +1,44 @@
 // 'https://jsonplaceholder.typicode.com/posts'
 import React, {useState, useEffect} from 'react'
 
-function Trial() {
-    //handle fetch data 
-    const [loadedResults, setLoadedResults] = useState([]);
 
-   useEffect(() =>{ 
-    const allResults = async () =>{
-      try{
-        const response = await fetch('https://randomuser.me/api/');
-        const {results} = await response.json();
-        setLoadedResults(results);
-        console.log(results);
+function Trial() {
+    
+    const [loadedResults, setLoadedResults] = useState([]);
+    
+      async function fetchData (){
+        try{
+          const response = await fetch('https://randomuser.me/api/?results=5000');
+          const results = await response.json();
+          console.log(results);
+          setLoadedResults(results);
+        }
+        catch(error){
+          console.log("Error Fetching data", error);
+        }
       }
-      catch(error){
-        console.log("Error Fetching data", error);
-      }
-    }
-     allResults();
-   
-   },[])
+      
+      useEffect(()=>{
+        fetchData();
+    }, []);
+     
+
 
   return (
     <div>
       <h1>All USERS FETCHED</h1>
-      <ul>
-        {loadedResults.map(({titile, name, cell,phone, picture}, index) =>(
+      <p>{loadedResults}</p>
+      {/* <ul>
+        {loadedResults.map(({titile, name, cell,phone, picture, email}, index) =>(
           <div key={index}>
             <h2>{name.title} {name.first} {name.last}</h2>
             <img src={picture.thumbnail} alt="User thumbnail" />
             <div>{cell}</div>
+            <p>{email}</p>
+            <button onClick={fetchData}>Fetch Data</button>
           </div>
         ))}
-      </ul>    
+      </ul>     */}
     </div>
    
    
