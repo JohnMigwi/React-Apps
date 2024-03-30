7e95fdd0a010079cb169fbc80e6e156ff6a39206
JohnMigwi <johnmigwi101@gmail.com>
2024-03-30 22:56:01 +0300

    stage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Post from "./Components/HTTP/post";
 import Pagination from "./Components/HTTP/pagination";
 import Parent from "./Optimazation/Parent";
+import Style from "./Components/Stylings/style";
 
 function App (){
   return (
     <>
-     <Pagination/>
-     <Parent/>
-     <Post/>
+     <Style/>
     </>
   )
 }
---
+++ src/Components/Stylings/style.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Style() {
+  return (
+    <div className='container'>
+        
+        <div id='text' className='Home'>Home</div>
+        <div id='text' className='about'>About</div>
+        <div id='text' className='Logout'>LogOut</div>
+    </div> 
+  )
+}
+
+export default  Style
